fe5f97f2e018965f62d715561550c402ee1272e7
Michael Gallaspy <gallaspy.michael@gmail.com>
2019-11-08 23:27:30 -0800

    Get table info and recursively search wiki links
    The spider now yields some basic table info, extracting the
    unadorned text from the header and data of each html table row.
    The title of the article is also returned.
    
    This spider recursively searches wiki links found in the body of the
    response as well, up to a limit determined by the spider definition.
    Currrently that limit is 1, so it only follows direct links in the
    first page.

--- scraping/mike/myspider.py
+++ scraping/mike/myspider.py
@@ -6,6 +6,11 @@ class WikipediaSpider(scrapy.Spider):
         'https://en.wikipedia.org/wiki/LinkedIn',
     ]
     
+    custom_settings = {
+        'DEPTH_LIMIT': 1, # Maximum depth to follow links
+        'ROBOTSTXT_OBEY': True,
+    }
+    
     def parse(self, response):
         title = response.css('h1::text').get()
         table_rows = response.xpath('//*[@id="mw-content-text"]/div/table[2]/*/tr')
@@ -14,8 +19,15 @@ class WikipediaSpider(scrapy.Spider):
             for tr in table_rows]
         self.log('Number of rows: %s ' % len(table_rows.getall()))
         self.log('Scraped  %s' % response.url)
+        
+        # Gets wiki links inside of the main content of the page
+        wiki_links = response.xpath("//*/div[starts-with(@id, 'content')]//*/a[starts-with(@href, '/wiki')]")
+        for link in wiki_links:
+            # print(link.attrib['href'])
+            yield response.follow(link)
+        
         yield {
-            'html': response.body,
+            # 'html': response.body.decode(),
             'table_data': table_data,
             'title': title,
             }
